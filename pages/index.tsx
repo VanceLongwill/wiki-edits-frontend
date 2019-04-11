8098bc5eb53711b98125d67963c92c555714d26f
Vance Longwill <vancelongwill@gmail.com>
2019-04-11 23:23:48 +0300

    feat: use nextjs head & link components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
 import Button from '../components/Button'
 import PageContainer from '../components/PageContainer'
+
 
 function App() {
   return (
     <PageContainer>
+      <Head>
+        <title>My page title</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+      </Head>
       <div>Welcome to Next.js!</div>
       <Button buttonText="some button" />
+      <br />
+      <Link href="#link-to-nowhere"><a>Some link</a></Link>
     </PageContainer>
   )
 }
