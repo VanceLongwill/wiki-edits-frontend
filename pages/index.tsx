de024aa485d3d6f6e7a70980d539c09dff9850ed
Vance Longwill <vancelongwill@gmail.com>
2019-04-12 00:03:35 +0300

    chore: run tslint autofix

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,21 @@ import Link from 'next/link'
 import Button from '../components/Button'
 import PageContainer from '../components/PageContainer'
 
-
-function App() {
+function App () {
   return (
     <PageContainer>
       <Head>
         <title>My page title</title>
         <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-          key="viewport"
+          name='viewport'
+          content='initial-scale=1.0, width=device-width'
+          key='viewport'
         />
       </Head>
       <div>Welcome to Next.js!</div>
-      <Button buttonText="some button" />
+      <Button buttonText='some button' />
       <br />
-      <Link href="#link-to-nowhere"><a>Some link</a></Link>
+      <Link href='#link-to-nowhere'><a>Some link</a></Link>
     </PageContainer>
   )
 }
